5b16e910262979250f53a01a6d37cdaa07310757
Gohe <juancarlos.gomez@ulv.edu.mx>
2026-05-03 02:22:29 -0500

    fix routing update

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,4 +1,4 @@
 export const ENV_ASIS = {
   apiUrl2 : 'http://localhost:3000/api',
-  apiUrl : 'https://backasiscontrol.onrender.com/api/'
+  apiUrl : 'https://backasiscontrol.onrender.com/api'
 };
