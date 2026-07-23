948672f7eb5189da1194fd58bd059ec40806a66f
Gohe <juancarlos.gomez@ulv.edu.mx>
2026-07-23 03:17:56 -0500

    fix(login): ennv

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,6 +1,6 @@
 export const ENV_ASIS = {
   apiUrl2 : 'http://localhost:3000/api',
-  apiUrl1 : 'https://backasiscontrol.onrender.com/api'
+  apiUrl : 'https://backasiscontrol.onrender.com/api'
 };
 
 export const ID_CLIENT = '218667265692-vuiapu3a4mlq69sublje0psss4kh4eq3.apps.googleusercontent.com';
