fc85038fbc8a2977d7aae267b4f0e79cab2afea4
Gohe <juancarlos.gomez@ulv.edu.mx>
2026-07-27 08:23:07 -0500

    Add security headersdd

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,6 +1,6 @@
 export const ENV_ASIS = {
-  apiUrl : 'http://localhost:3000/api',
-  apiUrl2 : 'https://backasiscontrol.onrender.com/api'
+  apiUrl2 : 'http://localhost:3000/api',
+  apiUrl : 'https://backasiscontrol.onrender.com/api'
 };
 
 export const ID_CLIENT = '218667265692-vuiapu3a4mlq69sublje0psss4kh4eq3.apps.googleusercontent.com';
